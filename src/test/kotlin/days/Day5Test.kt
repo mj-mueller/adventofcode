fbd1168ea773294fa8450b852347132cd2f73df7
Moritz Müller <contact@muellermoritz.com>
2020-12-21 17:26:40 +0100

    Improved day5 test

--- src/test/kotlin/days/Day5Test.kt
+++ src/test/kotlin/days/Day5Test.kt
@@ -10,4 +10,16 @@ internal class Day5Test {
         val day5 = Day5(ResourcesRequester.DAY_INPUT_PATH + "day5_sample.txt")
         assertEquals(357, day5.part1())
     }
+
+    @Test
+    fun testPart1() {
+        val day5 = Day5(ResourcesRequester.DAY_INPUT_PATH + "day5.txt")
+        assertEquals(926, day5.part1())
+    }
+
+    @Test
+    fun testpart2() {
+        val day5 = Day5(ResourcesRequester.DAY_INPUT_PATH + "day5.txt")
+        assertEquals(657, day5.part2())
+    }
 }
