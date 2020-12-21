b6c3612a53ea8f9df7adbd67e6212d642edec148
Moritz Müller <contact@muellermoritz.com>
2020-12-21 12:57:56 +0100

    Using getResource for files

---
+++ src/main/kotlin/days/utils/ResourcesRequester.kt
@@ -0,0 +1,20 @@
+package days.utils
+
+import org.apache.logging.log4j.LogManager;
+import java.net.URL
+
+class ResourcesRequester {
+    companion object {
+        const val DAY_INPUT_PATH = "/input/"
+        private val logger = LogManager.getLogger(ResourcesRequester::class.java.name)
+        fun getResource(filename: String): URL {
+            val resource: URL? = ResourcesRequester::class.java.getResource(filename)
+            if (resource == null) {
+                val e = IllegalArgumentException("Could not find resource $filename!")
+                logger.error("Error requesting resource", e)
+                throw e
+            }
+            return resource
+        }
+    }
+}
--- src/test/kotlin/days/Day2Test.kt
+++ src/test/kotlin/days/Day2Test.kt
@@ -1,12 +1,15 @@
 package days
 
+import days.utils.ResourcesRequester
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day2Test {
     @Test
     fun testSample() {
-        val day2 = Day2("./src/test/resources/input/day2_sample.txt")
+        val day2 = Day2(
+            ResourcesRequester.DAY_INPUT_PATH + "day2_sample.txt"
+        )
         assertEquals(2, day2.part1())
     }
 }
